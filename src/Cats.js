67a833ba96961365e00e6db6f1e6498bc8c23b60
Triana Cerda <trianaRcerda@gmail.com>
2021-08-17 09:48:12 -0700

    function warm up rendering + class deBugging

---
+++ src/Cats.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class cats extends Component {
+    render() { 
+        return ( 
+            <h1>
+                Cats
+            </h1>
+         );
+    }
+}
+ 
+export default cats;
---
+++ src/Dogs.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class dogs extends Component {
+    render() { 
+        return ( 
+            <h1>
+                Dogs
+            </h1>
+         );
+    }
+}
+ 
+export default dogs;
